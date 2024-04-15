73427a3dd09c381dd79c8e4dead2950255ce2d3d
crashRT <crashrt.farbauti@gmail.com>
2024-04-16 01:05:25 +0900

    case

--- chapter1/hello.go
+++ chapter1/hello.go
@@ -4,18 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	x := input("Type a price")
-	n, err := strconv.Atoi(x)
-	if err != nil {
-		fmt.Println("Invalid input")
-		return
+	x := 5
+	switch x {
+	case f(1):
+		fmt.Println("1")
+	case f(2):
+		fmt.Println("2")
+	case f(3):
+		fmt.Println("3")
+	default:
+		fmt.Println("default")
 	}
-	p := float64(n)
-	fmt.Println(int(p * 1.1))
+}
+
+func f(n int) int {
+	fmt.Println("No. ", n)
+	return n
 }
 
 func input(msg string) string {
