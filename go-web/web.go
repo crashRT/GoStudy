57aae388e06b17964934b6de0a2e661be88224d9
crashRT <crashrt.farbauti@gmail.com>
2024-08-12 22:48:55 +0900

    use template

--- go-web/web.go
+++ go-web/web.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	msg := `<h1>Hello, World!</h1>`
-	hh := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(msg))
+	html := `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Web</title>
+		</head>
+		<body>
+			<h1>Hello World!</h1>
+			<p> Go server is running.</p>
+		</body>
+	</html>`
+
+	tf, er := template.New("web").Parse(html)
+	if er != nil {
+		log.Fatal(er)
 	}
 
-	http.HandleFunc("/hello", hh)
+	hh := func(w http.ResponseWriter, r *http.Request) {
+		er = tf.Execute(w, nil)
+		if er != nil {
+			log.Fatal(er)
+		}
+	}
 
+	http.HandleFunc("/", hh)
 	http.ListenAndServe(":8080", nil)
 }
